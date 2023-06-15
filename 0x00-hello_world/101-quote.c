ab14ec93571e3d7c3b9c0286f487db8ddbf81d85
ximadoo <ikoudad995@gmail.com>
2023-06-15 07:36:56 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar,
+			2015-10-19\n", stdout);
+	return (1);
+}
