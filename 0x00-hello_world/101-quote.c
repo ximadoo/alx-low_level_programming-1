3cc633c829bace9104f121777122d71db35ebdd5
ximadoo <ikoudad995@gmail.com>
2023-06-15 07:44:20 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point of the program
  *
@@ -8,7 +7,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar,
-			2015-10-19\n", stdout);
-	return (1);
+    fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return (1);
 }
